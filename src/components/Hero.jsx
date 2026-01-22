9a5baa0fc202c531498b01cfe57724d4894e6314
Abdelrhman-Hussein <Abdelrhmanhussein007@gmail.com>
2026-01-22 11:45:22 +0200

    update hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import profileImage from '../assets/kevinRushProfile.jpg'
+import profileImage from '../assets/Abdelrhman-Hussein.jpeg'
 import { motion } from "motion/react"
 
 //handling motion
